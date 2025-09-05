a76edf96bde3ae4e6bff5fb512f5c85447d11cee
kyoppy <95670092+kyoppy-1229@users.noreply.github.com>
2025-09-06 04:24:14 +0900

    feat: add news article view (#8)

--- src/router.js
+++ src/router.js
@@ -3,12 +3,14 @@ import Home from './views/Home.vue'
 import GameRevivalPixi from './views/GameRevivalPixi.vue'
 import GameInitialPixi from './views/GameInitialPixi.vue'
 import BBSView from './views/BBSView.vue'
+import NewsArticle from './views/NewsArticle.vue'
 
 const routes = [
   { path: '/', name: 'home', component: Home },
   { path: '/revival', name: 'revival', component: GameRevivalPixi },
   { path: '/initial', name: 'initial', component: GameInitialPixi },
   { path: '/bbs', name: 'bbs', component: BBSView },
+  { path: '/news/20150302-17.html', name: 'news-20150302-17', component: NewsArticle },
 ]
 
 const router = createRouter({
