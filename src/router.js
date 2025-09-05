b2be0817e329bfafebc8fb6eb00e5d480dffc026
kyoppy <95670092+kyoppy-1229@users.noreply.github.com>
2025-09-06 04:06:49 +0900

    chore: remove hints from BBS log

--- src/router.js
+++ src/router.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from './views/Home.vue'
 import GameRevivalPixi from './views/GameRevivalPixi.vue'
 import GameInitialPixi from './views/GameInitialPixi.vue'
+import BBSView from './views/BBSView.vue'
 
 const routes = [
   { path: '/', name: 'home', component: Home },
   { path: '/revival', name: 'revival', component: GameRevivalPixi },
   { path: '/initial', name: 'initial', component: GameInitialPixi },
+  { path: '/bbs', name: 'bbs', component: BBSView },
 ]
 
 const router = createRouter({
